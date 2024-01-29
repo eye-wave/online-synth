6fcaa30378a65254ee24b56063dee50b6263de75
_eyewave <89079979+eye-wave@users.noreply.github.com>
2024-01-29 17:11:36 +0100

    add wav encoder

---
+++ src/wasm/io/io.test.ts
@@ -0,0 +1,23 @@
+import { $, write } from "bun"
+import { describe, test, expect } from "bun:test"
+import { get } from "svelte/store"
+import { globalStore } from "src/lib/global"
+import { Wavetables, IO } from "pkg/wavetable_synth"
+
+describe("Encoding wavetable to 'audio/wav' file", () => {
+  test("Sox should analyze the file wihout an error", async () => {
+    const freq = get(globalStore).BASE_FREQUENCY
+    const samplerate = 48000
+    const filepath = "/tmp/wavetable.wav"
+
+    const table = Wavetables.generate_basic_shapes_table(samplerate, freq)
+    const buffer = IO.encode_wav(table, samplerate)
+
+    await write(filepath, buffer)
+    const { exitCode } = await $`sox --info ${filepath}`
+
+    await $`rm ${filepath}`
+
+    expect(exitCode).toBe(0)
+  })
+})
