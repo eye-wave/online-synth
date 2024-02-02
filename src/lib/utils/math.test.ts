f536213ad5c13afb34ddd3b092593cd3c6adea3b
_eyewave <89079979+eye-wave@users.noreply.github.com>
2024-02-02 23:18:09 +0100

    chore format

--- src/lib/utils/math.test.ts
+++ src/lib/utils/math.test.ts
@@ -12,10 +12,10 @@ describe("clamp", () => {
 
 describe("round", () => {
   test("should round values", () => {
-    expect(round(1.21361544,0)).toEqual(1)
-    expect(round(8.79380892,2)).toEqual(8.79)
-    expect(round(6.12036598,3)).toEqual(6.120)
-    expect(round(9.84512032,4)).toEqual(9.8451)
-    expect(round(5.98412036,5)).toEqual(5.98412)
+    expect(round(1.21361544, 0)).toEqual(1)
+    expect(round(8.79380892, 2)).toEqual(8.79)
+    expect(round(6.12036598, 3)).toEqual(6.12)
+    expect(round(9.84512032, 4)).toEqual(9.8451)
+    expect(round(5.98412036, 5)).toEqual(5.98412)
   })
 })
